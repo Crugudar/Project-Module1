4d8962420a07dffb7e71f2a8bce7f8975bd057c6
Claudia D <claudia.do.gallego@gmail.com>
2020-10-13 21:08:14 +0200

    ya filtra

--- pueba.js
+++ pueba.js
@@ -26,54 +26,78 @@ function getInput(){
     var filtro2=document.getElementById('movieGenre').value;
     var filtro3=document.getElementById('Aname').value;
     var filtro4=document.getElementById('plot').value; 
+
     var r = [filtro1, filtro2, filtro3, filtro4];
+
+
+    return r;
     
-    return r; 
+    //.filter(item=>item);
+
+
+    
+    // return {
+    //     director_name:filtro1,
+    //     genres:filtro2,
+    //     actor_1_name:filtro3,
+    //     actor_2_name:filtro3,
+    //     actor_3_name:filtro3,
+    //     plot_keywords:filtro4
+    // }; 
 }
 
  
     
+
+
+
 let btnFil= document.getElementById('filter-btn');
 btnFil.addEventListener('click', async function (){
     
     const resultados = await getInput();
-     await filterArr(...resultados);
+     let prueba=await filterArr(resultados);
+
+     console.log(prueba);
     
        
-}); 
-
-
-    
+});   
 
 //función de filtrado
 
-async function filterArr(filtro1, filtro2, filtro3, filtro4){
+async function filterArr(filtro){
     let movies= await getMovies();
-console.log(filtro1, filtro2, filtro3, filtro4, 'vacaciones ya')
-
+        
+//  if(filtro[0]==''){console.log('filtro0 es una string vacía');}else{console.log('filtro0 NO es una string vacía');}
+//  if(filtro[0]==null){console.log('filtro0 es null');}else{console.log('filtro0 NO es null');}
     
     let filteredMovies= movies.filter(function(elem){
-        return ((filtro1 !==null && filtro1===elem.director_name)&&
-                (filtro2 !==null && elem.genres.includes(filtro2))&&
-                (filtro3 !==null && (elem.actor_1_name === filtro3 || elem.actor_2_name=== filtro3 || elem.actor_3_name=== filtro3))&&
-                (filtro4 !==null && elem.plot_keywords.includes(filtro4)));
+        if (filtro[0] != '' && filtro[0]!==elem.director_name) return false;
+        if (filtro[1] !='' && !elem.genres.includes(filtro[1])) return false;
+        if (filtro[2] !=='' && (elem.actor_1_name !== filtro[2] && elem.actor_2_name!== filtro[2] && elem.actor_3_name !== filtro[2])) return false;
+        if (filtro[3] !=='' && !elem.plot_keywords.includes(filtro[3])) return false;
+        
+        return true;
+
     });
 
    console.log(filteredMovies);
+   return filteredMovies;
 }
+
+
 
 //mostrar elementos filtrados en el HTML
 
-async function showMovies(){
-    let newContent= await filterArr();
+// async function showMovies(){
+//     let newContent= await filterArr();
     
-    let cartelera= await document.querySelector('.show-movies');
-    let newDiv=await document.createElement('div');
+//     let cartelera= await document.querySelector('.show-movies');
+//     let newDiv=await document.createElement('div');
     
-    await cartelera.appendChild(newDiv);
-    await newDiv.innerHTML(newContent);
-}
+//     await cartelera.appendChild(newDiv);
+//     await newDiv.innerHTML(newContent);
+// }
 
-console.log(showMovies());
+// console.log(showMovies());
 
 
